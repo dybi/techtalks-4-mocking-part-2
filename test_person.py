1be0eb0bd21c4709cc457a39edd9ae99987f27af
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-06-07 09:21:45 +0200

    Add a unit_test for make_friends with wrong patching place

---
+++ test_person.py
@@ -0,0 +1,12 @@
+from assertpy import assert_that
+from mock import patch
+
+from person import Person
+
+
+@patch("data_source.introduce_myself")
+def test_make_friends(mocked_introduce_function):
+    greeting = "howdy!"
+    mocked_introduce_function.return_value = greeting
+    p = Person("Borat")
+    assert_that(p.make_freinds()).is_equal_to(greeting)
