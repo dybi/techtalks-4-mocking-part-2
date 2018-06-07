d98aaa099f91c54e01c0090fca0f43e8b65f7b18
Piotr Dybowski <piotr.dybowski@codepoets.it>
2018-06-07 09:29:51 +0200

    Add Pet class and pet field to Person class

--- person.py
+++ person.py
@@ -4,6 +4,12 @@ from data_source import introduce_myself
 class Person(object):
     def __init__(self, name):
         self.name = name
+        self.pet = Pet()
 
     def make_freinds(self):
         return introduce_myself(self.name)
+
+
+class Pet(object):
+    def make_noise(self):
+        return "Woof!"
